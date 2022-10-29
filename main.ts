a31a1139185900c2fb63e33c2e53b450d323a070
bafreer <116974831+bafreer@users.noreply.github.com>
2022-10-29 16:33:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
 cuteBot.trackEvent(cuteBot.MbPins.Right, cuteBot.MbEvents.LoseLine, function () {
-    cuteBot.stopcar()
-    basic.pause(500)
-    left_wheel = left_wheel - 1
-    right_wheel = right_wheel + 1
+    basic.pause(10)
+    left_wheel = left_wheel - 5
+    right_wheel = right_wheel + 5
     cuteBot.motors(left_wheel, right_wheel)
 })
 cuteBot.trackEvent(cuteBot.MbPins.Left, cuteBot.MbEvents.LoseLine, function () {
-    cuteBot.stopcar()
-    basic.pause(500)
-    left_wheel = left_wheel + 1
-    right_wheel = right_wheel - 1
+    basic.pause(10)
+    left_wheel = left_wheel + 5
+    right_wheel = right_wheel - 5
     cuteBot.motors(left_wheel, right_wheel)
 })
 let left_wheel = 0
 let right_wheel = 0
 cuteBot.closeheadlights()
-right_wheel = 10
-left_wheel = 10
+right_wheel = 20
+left_wheel = 20
 if (cuteBot.trackSide(cuteBot.MbPins.Left, cuteBot.MbEvents.FindLine) && cuteBot.trackSide(cuteBot.MbPins.Right, cuteBot.MbEvents.FindLine)) {
     cuteBot.motors(left_wheel, right_wheel)
 }
